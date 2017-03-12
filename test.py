b712bfe3451b994295543691f55a08f21b3985e0
Stefano Posani <ste.posani@gmail.com>
2017-03-12 01:47:33 +0100

    asd

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ from random import randint
 def milanese(bot, update):
     testo = update.message.text.lower()
     print(testo)
-    print(update.message.text.from_user)
+    print(update.message.from_user)
     if (testo.find("milanese")!=-1):
-        update.message.reply_text('{}, Va a ciapà i ratt'.format(update.message.text.from_user),quote=False)
+        update.message.reply_text('{}, Va a ciapà i ratt'.format(update.message.from_user),quote=False)
 
 def rispostaFoto(bot, update):
     if (randint(0, 9) == 0):
