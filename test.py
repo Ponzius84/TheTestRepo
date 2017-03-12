3c89b860201c06acf3a91a2c22dd96e469b38eab
Stefano Posani <ste.posani@gmail.com>
2017-03-12 01:40:05 +0100

    aggiunta risposta randoma foto + insulto rat

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 # !/usr/bin/env python
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-def start(bot, update):
-    update.message.reply_text('Hi!')
+from random import randint
 
-def risposta(bot, update):
-    if (update.message.text.find("cazzone")!=-1):
-        update.message.reply_text('stronzo!',quote=False)
+def milanese(bot, update):
+    testo = update.message.text.lower()
+    if (testo.find("milanese")!=-1):
+        update.message.reply_text('{}, Va a ciapà i ratt'.format(update.message.text.from_user),quote=False)
 
 def rispostaFoto(bot, update):
-        update.message.reply_text('hai rotto la minchia')
+    if (randint(0, 9) == 0):
+        update.message.reply_text('Ue Avete finito o no, di postare foto come checche isteriche?',quote=false)
 
 
 TOKEN = "344707079:AAHSu8N5ibfJTsougIE-9-4UeTVZZ9DSWuM"
@@ -17,8 +18,9 @@ PORT = int(os.environ.get('PORT', '5000'))
 updater = Updater(TOKEN)
 # add handle
 dp = updater.dispatcher
-dp.add_handler(CommandHandler("start", start))
-dp.add_handler(MessageHandler(Filters.text,risposta))
+
+#Handles
+dp.add_handler(MessageHandler(Filters.text,milanese))
 dp.add_handler(MessageHandler(Filters.photo,rispostaFoto))
 
 updater.start_webhook(listen="0.0.0.0",
