e1ec9cf1660aa921b8a3d962b8eff6701ad2a0a6
Stefano Posani <ste.posani@gmail.com>
2017-03-13 01:43:22 +0100

    refactor

---
+++ functions.py
@@ -0,0 +1,49 @@
+from random import randint, choice
+
+def milanese(testo, update):
+    if "milanese" in testo:
+        update.message.reply_text('{}, Va a ciapà i ratt'.format(update.message.from_user.first_name), quote=False)
+
+def luxuria(testo, update):
+    if 'luxuria' in testo:
+        update.message.reply_photo(photo=open('images/vlad_{}.jpg'.format(randint(1, 6)), 'rb'), quote=False)
+        # update.message.reply_photo(photo="AgADAgAD-6cxG_ULMEo-AsCQeTsfrgk5Sw0ABKN_M80VTkK4Zx8GAAEC",quote=False)
+
+def domanda(testo, update):
+    risposte = ['ce la puoi fare',
+                'figa se asciughi',
+                'figa mollami']
+
+    if testo.find("?") != -1:
+        update.message.reply_text('{}, {}'.format(update.message.from_user.first_name, choice(risposte)), quote=False)
+
+def melina(testo, update):
+    risposte = ['Si lavora e si fatica per la panza e per la fica']
+    if testo.find("melina") != -1:
+        update.message.reply_text('{}'.format(choice(risposte)), quote=False)
+
+def saluto(testo, update):
+    saluti = ['ciao',
+              'buongiorno',
+              'buon giorno',
+              'salve']
+
+    risposte = ['we figa','we alura?']
+    if any(s in testo for s in saluti):
+        update.message.reply_text('{}'.format(choice(risposte)), quote=False)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def rispostaTesto(bot, update):
+    testo = update.message.text.lower()
+    print(update.message)
+    [f(testo, update) for f in [milanese, luxuria, domanda, melina, saluto]]
+
+
+
+def rispostaFoto(bot, update):
+    print(update.message)
+    n = randint(0, 9)
+    print(n)
+    if (n == 0):
+        update.message.reply_text('Ue Avete finito o no, di postare foto come checche isteriche?', quote=False)
--- milanese.py
+++ milanese.py
@@ -1,28 +1,7 @@
 # !/usr/bin/env python
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-from random import randint
-
-def milanese(bot, update):
-    testo = update.message.text.lower()
-    print(update.message)
-    print(os.listdir('.'))
-
-    if (testo.find("milanese")!=-1):
-        update.message.reply_text('{}, Va a ciapà i ratt'.format(update.message.from_user.first_name),quote=False)
-    elif (testo.find("luxuria") != -1):
-        update.message.reply_photo(photo=open('images/vlad_{}.jpg'.format(randint(1, 6)), 'rb'),quote=False)
-
-def rispostaFoto(bot, update):
-    print(update.message)
-    n = randint(0, 9)
-    print(n)
-    if (n == 0):
-        print("si smessaggia")
-        update.message.reply_text('Ue Avete finito o no, di postare foto come checche isteriche?',quote=False)
-
-
-
+from functions import rispostaTesto, rispostaFoto
 
 TOKEN = "344707079:AAHSu8N5ibfJTsougIE-9-4UeTVZZ9DSWuM"
 PORT = int(os.environ.get('PORT', '5000'))
@@ -31,11 +10,11 @@ updater = Updater(TOKEN)
 dp = updater.dispatcher
 
 #Handles
-dp.add_handler(MessageHandler(Filters.text,milanese))
+dp.add_handler(MessageHandler(Filters.text,rispostaTesto))
 dp.add_handler(MessageHandler(Filters.photo,rispostaFoto))
 
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
-updater.bot.setWebhook("https://milanese-bot.herokuapp.com/" + TOKEN)
+updater.bot.setWebhook("https://rispostaTesto-bot.herokuapp.com/" + TOKEN)
 updater.idle()
