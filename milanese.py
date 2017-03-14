9d4f34ed512604eb2c09a9a94695aa7c5ff9702e
Stefano Posani <ste.posani@gmail.com>
2017-03-14 01:54:06 +0100

    corretti degli import

--- milanese.py
+++ milanese.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-from functions import rispostaTesto, rispostaFoto
+from functions import Parser
 
 TOKEN = "344707079:AAHSu8N5ibfJTsougIE-9-4UeTVZZ9DSWuM"
 PORT = int(os.environ.get('PORT', '5000'))
@@ -9,9 +9,11 @@ updater = Updater(TOKEN)
 # add handle
 dp = updater.dispatcher
 
+p = Parser()
+
 #Handles
-dp.add_handler(MessageHandler(Filters.text,rispostaTesto))
-dp.add_handler(MessageHandler(Filters.photo,rispostaFoto))
+dp.add_handler(MessageHandler(Filters.text,p.rispostaTesto))
+dp.add_handler(MessageHandler(Filters.photo,p.rispostaFoto))
 
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
